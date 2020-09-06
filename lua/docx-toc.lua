6e9d3fd4a19e1f30ce07ea5a6905c8d141b9f115
ilcpm <ilcpm@outlook.com>
2020-09-07 03:29:10 +0800

    微调样式，输出项目介绍docx

--- lua/docx-toc.lua
+++ lua/docx-toc.lua
@@ -2,6 +2,14 @@ local RAW_TOC = [[
 <w:sdt>
     <w:sdtContent xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main">
         <w:p>
+            <w:pPr>
+                <w:pStyle w:val="TOC" />
+            </w:pPr>
+            <w:r>
+                <w:t xml:space="preserve">目录</w:t>
+            </w:r>
+        </w:p>
+        <w:p>
             <w:r>
                 <w:fldChar w:fldCharType="begin" w:dirty="true" />
                 <w:instrText xml:space="preserve">TOC \o "1-3" \h \z \u</w:instrText>
