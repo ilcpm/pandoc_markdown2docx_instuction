ee0cc6e55b2a60475d25bb20fbe8a379eba5e319
Hagb Green <hagb_green@qq.com>
2021-04-23 17:24:42 +0800

    Reduce unnecessary indent level

--- filters/docx_filters/equations_no.py
+++ filters/docx_filters/equations_no.py
@@ -27,100 +27,102 @@ class MathReplace():
 
     def action(self, elem, doc):
         # pf.debug('s:', elem)
-        if isinstance(elem, pf.Para):
+        if not isinstance(elem, pf.Para):
+            return
+        rows = []
+        content_group = []
+        for elem1 in elem.content:
+            if content_group and content_group[-1][0]:
+                if isinstance(elem1, (pf.Space, pf.SoftBreak)):
+                    continue
+                elif isinstance(elem1, pf.Str) and isinstance(
+                        content_group[-1][1][-1], pf.Math):
+                    if elem1.text == '{}':
+                        content_group[-1][1][-1] = (
+                            content_group[-1][1][-1], None)
+                        continue
+                    elif elem1.text == '{.notag}' or elem1.text == '{-}':
+                        content_group[-1][1][-1] = (
+                            content_group[-1][1][-1], "")
+                        continue
+                    else:
+                        match = self.anchor_re.fullmatch(elem1.text)
+                        if match:
+                            content_group[-1][1][-1] = (
+                                content_group[-1][1][-1], match[1])
+                            continue
+            is_math = isinstance(elem1,
+                                    pf.Math) and elem1.format == 'DisplayMath'
+            if content_group:
+                if content_group[-1][0] == is_math:
+                    content_group[-1][1].append(elem1)
+                    continue
+            content_group.append([is_math, [elem1]])
+        elem_old = elem
+        elem = []
+        for elem_group in content_group:
+            if not elem_group[0]:
+                elem_new = pf.Para(*elem_group[1])
+                elem_new.attributes = elem_old.attributes
+                elem_new.classes = elem_old.classes
+                elem_new.identifier = elem_old.identifier
+                elem.append(elem_new)
+                continue
             rows = []
-            content_group = []
-            for elem1 in elem.content:
-                if content_group and content_group[-1][0]:
-                    if isinstance(elem1, (pf.Space, pf.SoftBreak)):
-                        continue
-                    elif isinstance(elem1, pf.Str) and isinstance(
-                            content_group[-1][1][-1], pf.Math):
-                        if elem1.text == '{}':
-                            content_group[-1][1][-1] = (
-                                content_group[-1][1][-1], None)
-                            continue
-                        elif elem1.text == '{.notag}' or elem1.text == '{-}':
-                            content_group[-1][1][-1] = (
-                                content_group[-1][1][-1], "")
-                            continue
-                        else:
-                            match = self.anchor_re.fullmatch(elem1.text)
-                            if match:
-                                content_group[-1][1][-1] = (
-                                    content_group[-1][1][-1], match[1])
-                                continue
-                is_math = isinstance(elem1,
-                                     pf.Math) and elem1.format == 'DisplayMath'
-                if content_group:
-                    if content_group[-1][0] == is_math:
-                        content_group[-1][1].append(elem1)
-                        continue
-                content_group.append([is_math, [elem1]])
-            elem_old = elem
-            elem = []
-            for elem_group in content_group:
-                if elem_group[0]:
-                    rows = []
-                    for math_elem in elem_group[1]:
-                        if isinstance(math_elem, pf.Math):
-                            math_elem = math_elem
-                            notag = False
-                            tag = ''
-                        else:
-                            if isinstance(math_elem[1], str):
-                                if math_elem[1]:
-                                    tag = math_elem[1]
-                                    notag = False
-                                else:
-                                    tag = ''
-                                    notag = True
-                                    # pf.debug('notag')
-                                math_elem = math_elem[0]
-                            else:
-                                math_elem = math_elem[0]
-                                notag = False
-                                tag = ''
-                        math_caption = [
-                            pf.Span(pf.Str('('),
-                                    section_no,
-                                    pf.Str('.'),
-                                    equation_no,
-                                    pf.Str(')'),
-                                    identifier=tag)
-                        ] if not notag else []
-
-                        rows.append(
-                            pf.TableRow(
-                                pf.TableCell(
-                                    valign_block(50 * (1 - equation_width))),
-                                pf.TableCell(
-                                    valign_block(100 * equation_width),
-                                    pf.Div(pf.Para(math_elem),
-                                           attributes={
-                                               'custom-style': 'Equation'
-                                           })),
-                                pf.TableCell(
-                                    valign_block(50 * (1 - equation_width)),
-                                    pf.Div(pf.Para(*math_caption),
-                                           attributes={
-                                               'custom-style':
-                                               'Equation Caption'
-                                           }))))
-                        self.math_no += 1
-                    elem.append(
-                        pf.Table(pf.TableBody(*rows),
-                                 colspec=[
-                                     ('AlignLeft', (1 - equation_width) / 2),
-                                     ('AlignLeft', equation_width),
-                                     ('AlignRight', (1 - equation_width) / 2)
-                                 ],
-                                 caption=pf.Caption(pf.Div())))
+            for math_elem in elem_group[1]:
+                if isinstance(math_elem, pf.Math):
+                    math_elem = math_elem
+                    notag = False
+                    tag = ''
                 else:
-                    elem_new = copy.copy(elem_old)
-                    elem_new.content = elem_group[1]
-                    elem.append(elem_new)
-        # pf.debug('o:', elem)
+                    if isinstance(math_elem[1], str):
+                        if math_elem[1]:
+                            tag = math_elem[1]
+                            notag = False
+                        else:
+                            tag = ''
+                            notag = True
+                            # pf.debug('notag')
+                        math_elem = math_elem[0]
+                    else:
+                        math_elem = math_elem[0]
+                        notag = False
+                        tag = ''
+                math_caption = [
+                    pf.Span(pf.Str('('),
+                            section_no,
+                            pf.Str('.'),
+                            equation_no,
+                            pf.Str(')'),
+                            identifier=tag)
+                ] if not notag else []
+
+                rows.append(
+                    pf.TableRow(
+                        pf.TableCell(
+                            valign_block(50 * (1 - equation_width))),
+                        pf.TableCell(
+                            valign_block(100 * equation_width),
+                            pf.Div(pf.Para(math_elem),
+                                    attributes={
+                                        'custom-style': 'Equation'
+                                    })),
+                        pf.TableCell(
+                            valign_block(50 * (1 - equation_width)),
+                            pf.Div(pf.Para(*math_caption),
+                                    attributes={
+                                        'custom-style':
+                                        'Equation Caption'
+                                    }))))
+                self.math_no += 1
+            elem.append(
+                pf.Table(pf.TableBody(*rows),
+                            colspec=[
+                                ('AlignLeft', (1 - equation_width) / 2),
+                                ('AlignLeft', equation_width),
+                                ('AlignRight', (1 - equation_width) / 2)
+                            ],
+                            caption=pf.Caption(pf.Div())))
         return elem
 
     def __init__(self):
